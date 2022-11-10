cdca6a84a0a9a66901672f3439fb1c48763e7bf3
seroga213 <serogaseroga05@gmail.com>
2022-11-10 15:01:57 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,11 +15,10 @@ import { useState, useEffect } from "react";
 
 export const Phonebook = () => {
     const [contacts, setContacts] = useState(
-            [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }]);
-    const [filter, setFilter] = useState('');
+        () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
+      );
+      
+      const [filter, setFilter] = useState('')
 
 
 
